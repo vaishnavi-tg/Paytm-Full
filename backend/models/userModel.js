c1ce6a7aef4498b9e0124e9ceee4650ed9f1ca5c
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-22 10:49:04 +0530

    created mongoose model

---
+++ backend/models/userModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+})
+
+const User = mongoose.model("User",userSchema)
+
+export {User}
