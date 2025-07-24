d2ff0760671061259904540b6d6134e4765fdd03
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-24 09:57:40 +0530

    Created a http server using express

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express"
+import { config } from dotenv
+
+config()
+
+const app = express()
+
+try {
+    app.listen(process.env.PORT, () => {
+        console.log(`Listening on port ${process.env.PORT}`)
+    })
+} catch (error) {
+    console.log(error)
+}
+
+export default app
