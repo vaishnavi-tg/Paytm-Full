b910d5ad7c626587f96408def7e379400b238305
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-25 06:12:23 +0530

    added auth middleware

---
+++ backend/middleware.js
@@ -0,0 +1,24 @@
+import { JWT_SECRET } from "./config.js"
+import jwt from "jsonwebtoken"
+
+const authmiddledware = (req, res) => {
+    const authHeader = req.headers.authorization
+
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
+        res.status(403).json({})
+    }
+
+    const token = authHeader.split("")[1]
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET)
+
+        req.userId = decoded.userId
+        next()
+
+    } catch (error) {
+        return res.status(403).json({})
+    }
+}
+
+export { authmiddledware }
