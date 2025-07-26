ac3e45377b3d33b0cb1433756211b2dd4180ba63
vaishnavi-tg <vaishnavitg2@gmail.com>
2025-07-26 08:32:34 +0530

    Done backend

--- backend/controllers/accountContollers.js
+++ backend/controllers/accountContollers.js
@@ -5,8 +5,6 @@ const balance = async (req, res) => {
     const account = await Account.findOne({
         userId: req.userId
     })
-    console.log(account)
-    console.log(req.userId)
     res.json({
         balance: account.balance
     })
@@ -14,63 +12,35 @@ const balance = async (req, res) => {
 
 const transfer = async (req, res) => {
     const session = await mongoose.startSession();
+
     session.startTransaction();
+    const { amount, to } = req.body;
 
-    try {
-        let { amount, to } = req.body;
+    const account = await Account.findOne({ userId:req.userId }).session(session);
 
-        amount = Number(amount);
-        if (isNaN(amount) || amount <= 0) {
-            await session.abortTransaction();
-            return res.status(400).json({ message: "Invalid amount" });
-        }
-
-        if (!mongoose.isValidObjectId(to)) {
-            await session.abortTransaction();
-            return res.status(400).json({ message: "Invalid recipient userId format" });
-        }
-
-        // Validate sender's userId
-        if (!mongoose.isValidObjectId(req.userId)) {
-            await session.abortTransaction();
-            return res.status(400).json({ message: "Invalid sender userId format" });
-        }
-
-        const account = await Account.findOne({ userId: req.userId }).session(session);
-        if (!account || account.balance < amount) {
-            await session.abortTransaction();
-            return res.status(400).json({ message: "Insufficient Balance" });
-        }
-
-        const toAccount = await Account.findOne({ userId: to }).session(session);
-        if (!toAccount) {
-            await session.abortTransaction();
-            return res.status(400).json({ message: "Recipient account not found" });
-        }
-        
-
-        // Perform the transfer
-        await Account.updateOne(
-            { userId: req.userId },
-            { $inc: { balance: -amount } }
-        ).session(session);
-
-        await Account.updateOne(
-            { userId: to },
-            { $inc: { balance: amount } }
-        ).session(session);
-
-        await session.commitTransaction();
-        session.endSession();
-
-        res.json({ msg: "Transfer Successful" });
-
-    } catch (err) {
+    if (!account || account.balance < amount) {
         await session.abortTransaction();
-        session.endSession();
-        console.error("Transfer Error:", err);
-        res.status(500).json({ message: "Internal Server Error" });
+        return res.status(400).json({
+            message: "Insufficient balance"
+        });
     }
-};
 
+    const toAccount = await Account.findOne({ userId:req.body.to}).session(session);
+
+    if (!toAccount) {
+        await session.abortTransaction();
+        return res.status(400).json({
+            message: "Invalid account"
+        });
+    }
+
+    await Account.updateOne({ userId: req.userId }, { $inc: { balance: -amount } }).session(session);
+    await Account.updateOne({ userId: to }, { $inc: { balance: amount } }).session(session);
+
+    await session.commitTransaction();
+
+    res.json({
+        message: "Transfer successful"
+    })
+}
 export { balance, transfer }
